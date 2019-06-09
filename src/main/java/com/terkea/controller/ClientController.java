cee9480e378f0265ddec940eaaf3737a1fdc533e
Terkea <terkeabt@gmail.com>
2019-06-09 16:05:58 +0100

    display messages into the view

--- src/main/java/com/terkea/controller/ClientController.java
+++ src/main/java/com/terkea/controller/ClientController.java
@@ -1,39 +1,36 @@
 package com.terkea.controller;
 
 import com.terkea.model.Message;
+import javafx.application.Platform;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.ArrayList;
 
 public class ClientController {
-
-    private static String userName;
-
     @FXML
-    private ScrollPane allMessages;
+    private VBox testVBOX = new VBox();
 
     @FXML
     private TextArea typeMessage;
 
     @FXML
-    public TextArea muieDragnea = new TextArea();
-
-    @FXML
     private ScrollPane connectedUsersPane;
 
+    private static String userName;
     private static final int portNumber = 4444;
     public String host;
     private static Socket socket;
     private DataOutputStream out;
-    public ObservableList<Message> chat = FXCollections.observableArrayList();
+    public static ObservableList<Message> chat = FXCollections.observableArrayList();
 
     public String getHost() {
         return host;
@@ -52,41 +49,55 @@ public class ClientController {
     }
 
     @FXML
+    public void initialize(){
+        chat.addListener(new InvalidationListener() {
+            @Override
+            public void invalidated(Observable observable) {
+                Platform.runLater(()->{
+                    System.out.println("LAST MESSAGE: " + chat.get(chat.size()-1).getUserName() + " > " +chat.get(chat.size()-1).getMessage() );
+                    Label test = new Label(chat.get(chat.size()-1).getMessage());
+                    testVBOX.getChildren().add(test);
+                });
+            }
+        });
+    }
+
+
+    @FXML
     public void loadUser(String host, String userName){
+
+
         setUserName(userName);
         setHost(host);
         System.err.println(getUserName());
         System.err.println("HOST ADDRESS: " + getHost() + ":" +portNumber + "\n");
         System.out.println(getUserName() + " > Trying to connect to the server...");
+
         new Thread(() -> {
             createClient();
         }).start();
 
         System.out.println(getUserName() + " > Connected");
 
-        chat.addListener(new InvalidationListener() {
-            @Override
-            public void invalidated(Observable observable) {
-                System.out.println("Something happened");
-            }
-        });
+
 
     }
 
-
     @FXML
     public void createClient(){
+
         try {
             socket = new Socket(getHost(), portNumber);
 
             DataInputStream in = new DataInputStream(socket.getInputStream());
             new Thread(()->{
+
                 while(!socket.isClosed()){
                     try {
                         if (in.available() > 0){
                             String input = in.readUTF();
                             Message inputMessage = Message.fromJSON(input);
-                            System.out.println(inputMessage.getUserName() + " > " + inputMessage.getMessage());
+//                            System.out.println(inputMessage.getUserName() + " > " + inputMessage.getMessage());
                             chat.add(inputMessage);
                         }
                     } catch (IOException e) {
@@ -94,6 +105,7 @@ public class ClientController {
                     }
                 }
             }).start();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
